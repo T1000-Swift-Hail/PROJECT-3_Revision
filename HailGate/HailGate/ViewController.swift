af48abb6af3d730fd96c77aa5103f6a97a815e9e
Monafh <name@gmail.com>
2021-11-29 08:58:12 +0300

    First Screan >>

---
+++ HailGate/HailGate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HailGate
+//
+//  Created by Monafh on 24/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
