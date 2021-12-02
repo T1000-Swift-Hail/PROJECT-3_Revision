a45bc380b4189e75e8f0dfb532303f4d59be46ad
hana <hanooalshmmri3@gmail.com>
2021-12-02 10:29:10 +0300

    done

---
+++ Isolation22/Isolation2/extention/Ex.swift
@@ -0,0 +1,25 @@
+//
+//  Ex.swift
+//  Isolation
+//
+//  Created by hana Alshammri on 23/04/1443 AH.
+//
+
+import Foundation
+
+enum UserOptions {
+    case games
+    case books
+    case children
+    case HomeBusiness
+
+}
+
+enum TimeSelection {
+  case halfAnHour
+  case  Hour
+  case AnHourAndAHalf
+  case TwoHours
+    
+}
+
